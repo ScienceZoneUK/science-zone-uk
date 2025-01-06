e56c4a64e1d43e38f68dbe015470d0290bd3cc7f
ScienceZoneUK <sciencezoneuk@gmail.com>
2025-01-06 16:16:45 +0000

    Update sketch.js

--- p5js/new_yr_resolution/sketch.js
+++ p5js/new_yr_resolution/sketch.js
@@ -1,7 +1,18 @@
+res1 = "eat less sweets";
+res2 = "give maisie and bonnie more work to do";
+res3 = "Learn to defend myself against bonnie";
+res4 = "Surf more often";
+res5 = "IMprove python skills";
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  for (let i = 0; i < 5; i++) {
+    textSize(40);
+    let y = 40*i;
+    text(res2, 10, y);
+  }
 }
